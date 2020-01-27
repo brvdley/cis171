9c26bb28d1608bc5064113a70ab1f28b1d8296d4
Brvdley <bradleyowens126@gmail.com>
2020-01-27 10:36:23 -0600

    Weekly Class Update

---
+++ helloworld/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+	
+	public static void main (String[] args) {
+		int age = 19;
+		double gpa = 3.5;
+		int total = age + (int)gpa;
+		System.out.println("My age is: " + age + ".");
+		System.out.println();
+		System.out.println("This is a test.");
+		System.out.println();
+		System.out.println("Total is: " + total + ".");
+	}
+}
